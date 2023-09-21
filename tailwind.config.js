9d218b31d0d61a42e4f0d661f212508605c3da33
Gabriele Barberio <128740858+GabrieleBarberio@users.noreply.github.com>
2023-09-21 20:36:00 +0200

    build: added custom color to tailwind confing and set tailwind utilities in style.css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#07001F",
+      },
+    },
+  },
+  plugins: [],
+};
