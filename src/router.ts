c00412f1e3c3c1a344f3ab4743f1734157b4a879
Gabriele Barberio <128740858+GabrieleBarberio@users.noreply.github.com>
2023-09-21 21:02:30 +0200

    build: added path to render car datails component

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 
 import CarsList from './components/CarsList.vue';
+import CarDetails from './components/CarDetails.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     component: CarsList,
-  }
+  },
+  {
+    path: '/car/:id',
+    component: CarDetails,
+    props: true,
+  },
 ];
 
 const router = createRouter({
