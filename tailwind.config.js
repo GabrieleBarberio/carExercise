579897b9ed7259c33d83378feb8bb2ac01ff57cb
Gabriele Barberio <128740858+GabrieleBarberio@users.noreply.github.com>
2023-09-21 20:38:03 +0200

    build: added montserrat font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
       colors: {
         dark: "#07001F",
       },
